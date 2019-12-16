aa8215ae3648532568a82ffd9dda975ac98c75b6
sakaryag <39420109+sakaryag@users.noreply.github.com>
2019-12-16 23:38:21 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,30 +1,50 @@
-from earthquake import earthquake
+import sqlite3 as dbapi2
+
+from earthquake import Earthquake
 
 
 class Database:
-    def __init__(self):
-        self.earthquake = {}
-        self._last_earthquake_key = 0
+    def __init__(self, dbfile):
+        self.dbfile = dbfile
 
     def add_earthquake(self, earthquake):
-        self._last_earthquake_key += 1
-        self.earthquake[self._last_earthquake_key] = earthquake
-        return self._last_earthquake_key
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO EQ (TITLE, YR) VALUES (?, ?)"
+            cursor.execute(query, (earthquake.title, earthquake.year))
+            connection.commit()
+            earthquake_key = cursor.lastrowid
+        return earthquake_key
+
+    def update_earthquake(self, earthquake_key, earthquake):
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE EQ SET TITLE = ?, YR = ? WHERE (ID = ?)"
+            cursor.execute(query, (earthquake.title, earthquake.year, earthquake_key))
+            connection.commit()
 
     def delete_earthquake(self, earthquake_key):
-        if earthquake_key in self.earthquake:
-            del self.earthquake[earthquake_key]
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM EQ WHERE (ID = ?)"
+            cursor.execute(query, (earthquake_key,))
+            connection.commit()
 
     def get_earthquake(self, earthquake_key):
-        earthquake = self.earthquakes.get(earthquake_key)
-        if earthquake is None:
-            return None
-        earthquake_ = earthquake(earthquake.title, year=earthquake.year)
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT TITLE, YR FROM EQ WHERE (ID = ?)"
+            cursor.execute(query, (earthquake_key,))
+            title, year = cursor.fetchone()
+        earthquake_ = Earthquake(title, year=year)
         return earthquake_
 
     def get_earthquakes(self):
         earthquakes = []
-        for earthquake_key, earthquake in self.earthquakes.items():
-            earthquake_ = earthquake(earthquake.title, year=earthquake.year)
-            earthquakes.append((earthquake_key, earthquake_))
+        with dbapi2.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "SELECT ID, TITLE, YR FROM EQ ORDER BY ID"
+            cursor.execute(query)
+            for earthquake_key, title, year in cursor:
+                earthquakes.append((earthquake_key, Earthquake(title, year)))
         return earthquakes
